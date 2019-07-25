04e7f5013f73600eacca6e4cac95f1be3ce2da16
shaoyun <qazxsw9295@gmail.com>
2019-07-25 11:34:40 +0800

    2. Token Transfer

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,12 +1,30 @@
-pragma solidity ^0.4.2;
+pragma solidity ^0.4.24;
 
 contract DappToken{
-
+	string public name = "DappToken";
+	string public symbol = "DAPP";
+	string public standard = "DappToken v1.0";
 	uint256 public totalSupply;
-	//constructor
+	
+	event Transfer(
+		address indexed _from,
+		address indexed _to,
+		uint256 _value
+		);																							
+	mapping(address=>uint256) public balanceOf;
 
-	function DappToken() public {
-		totalSupply = 1000000;
+	constructor(uint256 _initialSupply) public {
+		balanceOf[msg.sender] = _initialSupply;
+		totalSupply = _initialSupply;
+		//allocate the initial supply
 
+	}
+
+	function transfer(address _to, uint256 _value) public returns(bool success){
+		require(balanceOf[msg.sender] >= _value);
+		balanceOf[msg.sender] -= _value;
+		balanceOf[_to] +=  _value;
+		Transfer(msg.sender, _to, _value);
+		return true;
 	}
 }
